d2204a466ced70e473a61ba7ae671273c51787d6
Zack W. Almquist <almquist@uci.edu>
2009-11-11 00:00:00 +0000

    version 0.05

--- R/demToNum.R
+++ R/demToNum.R
@@ -3,10 +3,9 @@ demToNum<-function(sp.object=NULL){
 	if(class(sp.object)[1]!="SpatialPolygonsDataFrame")
 		cat("Incompatible class!")
 	
-	b<-which(names(sp.object)=="pop2000")
-	e<-ncol(sp.object@data)
-	for(i in 4:89){
-	sp.object@data[,i]<-as.numeric(sp.object@data[,i])
-	}
-	sp.object
+	b<-which(names(sp.object)=="pop2000") ## Grab first index
+	e<-ncol(sp.object@data) ## Grab last index
+	temp<-apply(sp.object@data[,b:e],2,as.numeric) ## Convert
+	sp.object@data[,b:e]<-temp	## Replace
+	sp.object ## Return
 	}
